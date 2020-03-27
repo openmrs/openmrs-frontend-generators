cb36bdc26fde1f611ce893471e26a303a8790f4a
Brandon Istenes <bistenes@gmail.com>
2020-03-27 15:23:36 -0700

    Fix tests

--- packages/create-openmrs/__tests__/create-openmrs.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const createOpenmrs = require('..');
-
-describe('create-openmrs', () => {
-    it('needs tests');
-});
--- packages/generator-openmrs/test/generator-openmrs.test.js
+++ packages/generator-openmrs/test/generator-openmrs.test.js
@@ -1,7 +1,32 @@
-'use strict';
+const path = require("path");
+const generator = require("../src/generator-openmrs");
+const helpers = require("yeoman-test");
+const assert = require("yeoman-assert");
 
-const generatorOpenmrs = require('..');
+describe("generator-openmrs", () => {
+  let runContext;
 
-describe('generator-openmrs', () => {
-    it('needs tests');
+  afterEach(() => {
+    runContext.cleanTestDirectory();
+  });
+
+  it("can run the generator", () => {
+    runContext = helpers
+      .run(generator)
+      .withGenerators([
+        [
+          helpers.createDummyGenerator(),
+          "generator-single-spa/src/generator-single-spa"
+        ]
+      ])
+      .withPrompts({
+        moduleType: "app-parcel",
+        framework: "react",
+        packageManager: "yarn"
+      });
+
+    return runContext.then(dir => {
+      assert.file(path.join(dir, "package.json"));
+    });
+  });
 });
